c791bcca1d02259cb3108db70c413ebcd8b77456
Shahin-Alz <shaalemzadeh@gmail.com>
2022-12-20 14:04:34 +0100

    ajout

---
+++ public/js/main.js
@@ -0,0 +1,69 @@
+// 1
+
+// let positif = 1
+// let negatif = "1"
+
+// console.log(positif == negatif);
+
+// 2
+
+// let positif = 1
+// let negatif = "1"
+// console.log(positif === negatif);
+
+// 3
+
+// let prenom = prompt("un prenom?");
+
+// if (prenom.length < 6) {
+
+// alert("ton prenom est trop court")
+    
+// }
+
+// 4
+
+// let uno = prompt("50 + 50 font cmb?")
+
+// if (uno == 100) {
+//     alert("felicitation");
+// }
+// else{
+//     alert("incorrect")
+// }
+
+// 5 (pas bien effectuer)
+
+// let uno = prompt("5 * 5 font cmb?")
+// let dos = uno - 25
+
+// if (uno == 25) {
+// alert("felicitation");
+// }
+// else{
+//     alert( dos );
+// }
+
+// 6
+
+// let uno = prompt("premier mot")
+// let dos=  prompt("deuxieme mot")
+// let tres= prompt("troisieme mot")
+
+
+// let tableau = [uno, dos, tres]
+
+// if (tableau.length == 3) {
+//     alert(tableau)
+// }
+
+// 7 pas réussi
+
+// let nombre= prompt("nombre")
+// let grandnombre = [nombre >=12 ]
+// let petitnombre = [nombre <=12 ]
+
+// alert(grandnombre, petitnombre)
+
+
+
